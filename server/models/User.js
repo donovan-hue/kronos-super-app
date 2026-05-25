20e14490789f2216c2e71e10d3a65ac5c8981e78
donovan-hue <doonovaanga@gmail.com>
2026-05-24 22:51:56 -0600

    fix: add return to password hash pre-save hook — passwords were being corrupted on save

--- server/models/User.js
+++ server/models/User.js
@@ -134,12 +134,13 @@ userSchema.pre('save', function (next) {
 
 // Hash password antes de guardar
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) {
-    next();
+  if (!this.isModified('password') || !this.password) {
+    return next();
   }
 
   const salt = await bcryptjs.genSalt(10);
   this.password = await bcryptjs.hash(this.password, salt);
+  next();
 });
 
 // Comparar contraseñas
