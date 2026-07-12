ec5340d91c50ca65af0ac7ccc052ab14d60c2f5c
Donovan-hue <doonovaanga@gmail.com>
2026-07-11 22:29:51 -0600

    Limpieza: eliminar respaldos y archivos temporales

--- backups/orchestrator-20260710-220704/test-orchestrator.js
+++
@@ -1,7 +0,0 @@
-const orchestrator = require("./index");
-
-orchestrator.initialize();
-
-console.log(orchestrator.getAgent(100).name);
-console.log(orchestrator.getGroup("group-03").length);
-console.log(orchestrator.getAllAgents().length);
