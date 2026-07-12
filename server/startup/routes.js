f1bb4aa8d9b4dbec6ab597af284e4cf8ea78d220
Donovan-hue <doonovaanga@gmail.com>
2026-07-12 04:26:15 -0600

    Paquete 13: agregar startup/routes

---
+++ server/startup/routes.js
@@ -0,0 +1,11 @@
+module.exports = function configureRoutes(app) {
+  // Aquí moveremos gradualmente todas las rutas:
+  //
+  // app.use('/api/auth', ...)
+  // app.use('/api/posts', ...)
+  // app.use('/api/users', ...)
+  // app.use('/api/messages', ...)
+  // etc.
+  //
+  return app;
+};
